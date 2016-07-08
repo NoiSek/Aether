ef5cff6564ae18b5863d24874e598279eacd4007
Noi Sek <noi.t.sek@gmail.com>
2016-07-07 22:11:59 -0700

    Testing localstorage.

--- src/es6/Main.jsx
+++ src/es6/Main.jsx
@@ -8,8 +8,20 @@ import { LoginPanel } from './LoginPanel';
 import { Clock } from './Clock';
 
 export default function Main() {
+  let backgrounds = ['abstract-1.jpg', 'boko.jpg', 'car.jpg', 'cavern.jpg', 'dark-grass.jpg', 'frosted.jpg', 'jet.png', 'mountains-1.jpg', 'night.png', 'ocean.jpg', 'paper-lanterns.jpg', 'pier.jpg', 'sanfran.jpg', 'space-1.jpg', 'space-3.jpg', 'space-4.jpg', 'tree-2.jpg'];
+  
   // Add notifications to the global scope for error handling
   window.notifications = new Notifications();
+
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.getItem("wallpaper") === null) {
+      localStorage.setItem("wallpaper", "space-1.jpg");  
+    } else {
+      // Stuff
+    }    
+  } else {
+    window.notifications.generate("localStorage not supported.", 'error');
+  }
 
   InfernoDOM.render(<CommandPanel />, document.getElementById("command-panel"))
   InfernoDOM.render(<LoginPanel />, document.getElementById("login-panel"));
--- src/js/Main.js
+++ src/js/Main.js
@@ -31,8 +31,20 @@ define(['exports', 'src/dist/js/inferno.min', 'src/dist/js/inferno-component.min
   });
 
   function Main() {
+    var backgrounds = ['abstract-1.jpg', 'boko.jpg', 'car.jpg', 'cavern.jpg', 'dark-grass.jpg', 'frosted.jpg', 'jet.png', 'mountains-1.jpg', 'night.png', 'ocean.jpg', 'paper-lanterns.jpg', 'pier.jpg', 'sanfran.jpg', 'space-1.jpg', 'space-3.jpg', 'space-4.jpg', 'tree-2.jpg'];
+
     // Add notifications to the global scope for error handling
     window.notifications = new _Notifications.Notifications();
+
+    if (typeof Storage !== "undefined") {
+      if (localStorage.getItem("wallpaper") === null) {
+        localStorage.setItem("wallpaper", "space-1.jpg");
+      } else {
+        // Stuff
+      }
+    } else {
+      window.notifications.generate("localStorage not supported.", 'error');
+    }
 
     _infernoDom2.default.render(bp0(_CommandPanel.CommandPanel), document.getElementById("command-panel"));
     _infernoDom2.default.render(bp1(_LoginPanel.LoginPanel), document.getElementById("login-panel"));
