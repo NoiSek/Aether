ce347c3bfbb3082a980d7b51566275a9eda7db1b
Noi Sek <noi.t.sek@gmail.com>
2019-01-20 00:12:31 -0800

    Tinker with experimental starfield values.

--- src/js/Components/ExperimentalStars/Particle.js
+++ src/js/Components/ExperimentalStars/Particle.js
@@ -19,23 +19,22 @@ export default class Particle {
 
 
   recycle() {
-    const parentVelocity = this.options.parent[0];
-    const parentSprite = this.options.parent[1];
+    const parentVelocity = this.options.parent.velocity;
 
     // Reset Particle
     this.birthDate = Number(new Date());
-    this.velocity = [ -(parentVelocity[0]) / 100, -(parentVelocity[1]) / 100 ];
+    this.velocity = [ -(parentVelocity.x) / 100, -(parentVelocity.y) / 100 ];
     this.lifetime = randomRange(this.options.minDecay, this.options.maxDecay, 0);
     this.elapsed = 0;
 
     // Reset Sprite
     this.sprite.scale.set(this.options.startScale);
-    this.sprite.alpha = 0.05;
+    this.sprite.alpha = 0;
     this.sprite.tint = this.options.startColor;
     this.sprite.rotation = randomRange(-10, 10, 0) * Math.PI / 180;
 
     let ratio = randomRange(0.5, 1, 2);
-    let interpolatedPosition = parentSprite.interpolate(ratio);
+    let interpolatedPosition = this.options.parent.interpolate(ratio, 1.25);
 
     this.sprite.x = interpolatedPosition.x + 2;
     this.sprite.y = interpolatedPosition.y;
@@ -56,7 +55,12 @@ export default class Particle {
       this.sprite.x += this.velocity[0];
       this.sprite.y += this.velocity[1];
 
-      this.sprite.alpha = (1 - percentage) / 10;
+      let scaledPercentage = scale(0, 3.14, percentage, false);
+      let rawAlpha = Math.sin(scaledPercentage);
+      let scaledAlpha = scale(0, 0.15, rawAlpha, false);
+
+      this.sprite.alpha = scaledAlpha;
+
       this.sprite.tint = this.options.gradientGenerator(percentage);
     }
   }
